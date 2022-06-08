b01a5a3d1f289948e0c9c5beb8193c410fedb281
Luiz Henrique <luizhenrique92sp@gmail.com>
2022-06-08 15:06:25 -0300

    3º dia do 7Days e esse sofri um pouco, fiquei tentando códigos loucos até que percebi que o mais simples era o melhor a se fazer

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ if (numeroDez == stringDez) {
 }
 */
 
-
+/*
 // #7DaysOfCode - Dia 2 - Váriaveis 
 const texto = document.querySelector("#texto")
 const texto2 = document.querySelector("#texto2")
@@ -48,5 +48,46 @@ console.log(texto.innerHTML)
   if(pergunta == 2) {
     texto2.innerHTML = `Ahh que pena que não está gostando de estudar ${linguagem} ${nome}... Já tentou aprender outras linguagens?`
   }
+*/
+
+
+
+// #7DaysOfCode - Dia 3 - Controle de fluxo
+const texto = document.querySelector("#texto")
+const texto2 = document.querySelector("#texto2")
+
+
+const areaInicial = prompt("Qual área gostaria de seguir? `Front-End` ou `Back-End`?")
+let dominio = ""
+
+if(areaInicial === "Front-End") {
+  dominio = prompt(`Que bom que escolheu seguir com ${areaInicial}, agora gostaria de aprender "React" ou "Vue"?`)
+} else if(areaInicial === "Back-End") {
+  dominio = prompt(`Que bom que escolheu seguir com ${areaInicial}, agora gostaria de aprender "C#" ou "Java"?`)
+} else {
+  dominio = prompt("Você provavelmente digitou algo errado!!")
+}
+
+
+const FullOuEspe = prompt(`Gostaria de se "Especializar" em ${areaInicial} ou gostaria de seguir se desenvolvendo para se tornar "Fullstack"?`)
+
+if(FullOuEspe === "Especializar") {
+  alert(`Continue se especializando em ${areaInicial}, ainda tem muita coisa para se aprender em ${dominio}!!!`)
+} else if (FullOuEspe === "Fullstack"){
+  alert(`Explore o mundo além do ${areaInicial}, existem muitas outras linguagens para conhecer!!!`)
+}
+
+const perguntaFinal = prompt("Ainda existe outra tecnologia que gostaria de conhecer? Responda ok se sim!!!")
+
+for(i = "ok"; perguntaFinal === i;) {
+  let qual = prompt("Qual seria?")
+  alert(`Muito bem, ${qual} é muito interessante de se aprender!`)
+  const perguntaFinal = prompt("Ainda existe outra tecnologia que gostaria de conhecer? Responda ok se sim!!!")
+  if(perguntaFinal === "não") {
+    break
+  }
+}
+
+
 
 
